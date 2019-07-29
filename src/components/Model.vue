c4809e79ee4c5637bb26c6350df2103e73fa3331
goshva <sha.egor@gmail.com>
2019-07-29 09:04:01 +0300

    let go

---
+++ src/components/Model.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="models">  
+    <h1 > list{{ msg }}</h1>
+    <div v-for="model in models" class="hello">
+      {{model.name}}
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Model',
+  props: {
+    msg: String
+  },
+  computed: {
+    isLoggedIn: function() {
+      return this.$store.getters.isLoggedIn;
+    },
+    models: function() {
+      return this.$store.getters.models;
+    }
+  },
+    mounted: function(){
+        this.list();
+    },  
+  methods: {
+    list: function() {
+      this.$store
+        .dispatch("list")
+    //    .then(() => this.model = this.$store.getters.models)
+        .catch(err => console.log(err));
+    }
+  }
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Model msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Model from '@/components/Model.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Model
+  }
+}
+</script>
