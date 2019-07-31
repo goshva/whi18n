2ef1cdebe18e8ed3a03d1284e7d792a3777b8a18
goshva <sha.egor@gmail.com>
2019-07-31 17:45:25 +0300

    color to default

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -45,6 +45,12 @@ export default {
     border-bottom: 1px  solid #eee
 input
     border: none
+input:-webkit-autofill,
+input:-webkit-autofill:hover, 
+input:-webkit-autofill:focus, 
+input:-webkit-autofill:active 
+	-webkit-box-shadow: 0 0 0 30px white inset !important 
+    
 button
   border: none
   border-bottom: 1px  solid #ccc
